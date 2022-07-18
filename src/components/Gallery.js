19f086659724e9f1f1ce202ab9ae717c63dc49b5
Alex Cernik <devcernik@gmail.com>
2022-07-18 20:52:33 -0300

    gray scale image

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -11,8 +11,8 @@ const Gallery = () => {
       <Box id='2' mb={10}>
         <Typography textAlign='center' fontWeight='bold' variant="h5" component='h3'>Mis proyectos</Typography>
         <ImageList style={{ width: '100%', height: '100%', paddingBottom: 10 }} gap={8} cols={xsUp ? 1 : lgDown ? 2 : 3}>
-          {itemData.map((item) => (
-            <ImageListItem className='img-hover' key={item.img} style={{ height: 260, borderRadius: 6, boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)' }}>
+          {itemData.map((item, index) => (
+            <ImageListItem key={index} className='img-hover' style={{ height: 260, borderRadius: 6, boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)' }}>
               <img
                 src={item.img}
                 alt={item.title}
