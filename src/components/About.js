4d00bf8af521d1b83a1c2d90611d115039dc359c
Alex Cernik <devcernik@gmail.com>
2022-07-19 16:23:11 -0300

    change style of scrollbar

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ const Abount = () => {
       <Stack id='1' direction={{ xs: 'column', sm: 'row' }} pt={{ xs: 10, sm: 20 }} pb={{ xs: 10, sm: 20 }} pl={{ xs: 1, sm: 2, md: 15 }} pr={{ xs: 1, sm: 2, md: 15 }} spacing={3} alignItems={{ xs: 'center', sm: 'flex-start' }} justifyContent='center'>
         <Avatar alt='name' src='https://mui.com/static/images/avatar/1.jpg' style={{ width: 140, height: 140, boxShadow: '0px 2px 35px 1px rgba(0,0,0,0.9)', backgroundColor: 'rgba(0,0,0,0.2)' }} />
         <Box width='100%'>
-          <Typography className='name-personal' variant="h4" component='h1' fontWeight='bold' textAlign={{ xs: 'center', sm: 'left' }}>Pablo Hajnal</Typography>
-          <Typography variant="h6" component='p' fontSize={16} color='#e0e0e0'>
+          <Typography mt={1} className='name-personal' variant="h4" component='h1' fontWeight='bold' textAlign={{ xs: 'center', sm: 'left' }}>Pablo Hajnal</Typography>
+          <Typography mt={1} ml={{ xs: 0, sm: 1 }} variant="h6" component='p' fontSize={16} color='#e0e0e0'>
             Lorem ipsum dolor sit amet. Et sunt dolorem sed consequatur tempore et quia
             vero ea facere iusto! Hic voluptatem error et dolorem tempora qui dolorum
             laborum 33 minima magni in sint autem et quaerat nihil. Qui enim magni et
